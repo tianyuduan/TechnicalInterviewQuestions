92c0436d73bd5d63b48630327a943568a6239c2b
Tianyu Duan <tianyuduan1991@gmail.com>
2017-11-14 18:21:25 -0800

    pair sum most effecient

--- algs.rb
+++ algs.rb
@@ -12,3 +12,36 @@ end
 # 123125.reverse
 
 # p 12345.to_s.split("")
+
+
+# given two numbers , return a boolean T/F stating whether two numbers in an array add up to the given numbers
+
+# first approach is using two iterative O(x^2) approach finding every possible sum
+# secondapporeach would be using binary search approach
+
+# the third approach
+
+
+def has_pair_with_sum(arr, sum)
+
+  low = 0
+  high =
+
+
+end
+
+
+has_pair_with_sum([1,2,3,9], 8)
+
+
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
